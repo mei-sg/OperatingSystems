d9601d11a0cc675f760690b759167dfcdf66d517
Mei <mjoycesg@gmail.com>
2026-03-06 01:44:20 -0800

    More progress again

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
+/* ============================ INCLUDE HEADER FILES ============================ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include<pthread.h> //for multi-thread related system call
-#include<sys/types.h> //thread id type “pthread 
+#include <pthread.h>
+#include <sys/types.h>
+#include "queue.h"
+#include <unistd.h>
 
-// Global variables
+/* ============================ CONSTANTS ============================ */
+#define TRUE 1
+#define FALSE 0
+
 #define EMPTY -1
 #define BUSINESS 0
 #define ECONOMY 1
 #define NQUEUE 2
 #define NCLERKS 5
 #define MAX_CUSTOMERS 100
-#define TRUE 1
-#define FALSE 0
 
-// Struct to record customer information read from customers.txt
+/* ============================ DATA STRUCTURES ============================ */
 struct customerInfo {
     int userID;
 	int classType;
@@ -22,49 +26,59 @@ struct customerInfo {
 	int arrivalTime;
 };
 
+/* ============================ CUSTOMER DATA ============================ */
 // Customer list from file parsing
 struct customerInfo *customerDetails; 
 int totalCustomers;
+int customersServed = 0;
 
-// Queue variables
-int queue[NQUEUE][MAX_CUSTOMERS]; // Make two queues
-int front[NQUEUE]; // Keep track of queue front
-int back[NQUEUE]; // Keep track of queue back
-int queueLength[NQUEUE]; // Queue length for BUSINESS and ECONOMY
-int queueStatus[NQUEUE]; // Variable to record the status of a queue, the value could be idle (not using by any clerk) or the clerk id (1 ~ 4), indicating that the corresponding clerk is now signaling this queue.
-int winnerSelected[NQUEUE] = {FALSE}; // Variable to record if the first customer in a queue has been successfully selected and left the queue.
+/* ============================ QUEUE STATE ============================ */
+// Which clerk is currently serving the queue (there could be no clerk serving the queue)
+int queueStatus[NQUEUE];
 
+// Record if the first customer in a queue has been selected and left the queue
+int winnerSelected[NQUEUE] = {FALSE};
 
-// Time variables 
-struct timeval initTime; // Use this variable to record the simulation start time; No need to use mutex_lock when reading this variable since the value would not be changed by thread once the initial time was set.
-double overallWaitTime; //A global variable to add up the overall waiting time for all customers, every customer add their own waiting time to this variable, mutex_lock is necessary.
+/* ============================ SIMULATION TIME ============================ */
+struct timeval initTime; // Record the simulation start time
+double overallWaitTime; //Overall waiting time for all customers
 
-
-// thread variables
+/* ============================ THREAD SYNCHRONIZATION ============================ */
+// Mutexes and condition variables for BUSINESS and ECONOMY queues
 pthread_mutex_t queueMutex[NQUEUE];
 pthread_cond_t queueCond[NQUEUE];
 
-pthread_t clerkThreads[NCLERKS];
+// Mutexes and condition variables for clerks
+pthread_t clerkThread[NCLERKS];
 pthread_mutex_t clerkMutex[NCLERKS];
 pthread_cond_t clerkCond[NCLERKS];
 
+// Mutex for overall wait time
 pthread_mutex_t overallWaitMutex;
 
+// Mutex for number of customers served
+pthread_mutex_t customersServedMutex;
+
+/* ============================ FUNCTION PROTOTYPES ============================ */
+struct customerInfo* readFile(char *filename, int *numCustomers);
+void enqueue(int queueType, int customerIndex);
+int dequeue(int queueType);
+int serveQueue(void);
+double getCurrentTime(void);
+void updateWaitTime(double waitTime);
+void* customerEntry(void *customerArg);
+void* clerkEntry(void *clerkArg);
 
 
-
-
-
+/* +++++++++++++++++++++++ MAIN FUNCTION +++++++++++++++++++++++ */
 
 int main() {
 
+/* ============================ INITIALIZATIONS ============================ */
 	// Initialize the queues
+	initQueues();
 	for (int i = 0; i < NQUEUE; i++) {
-    	front[i] = 0;
-    	back[i] = 0;
-    	queueLength[i] = 0;
-    	winnerSelected[i] = FALSE;
-    	queueStatus[i] = 0;
+    	queueStatus[i] = EMPTY;
 	}
 
 	// Initialize queue mutexes
@@ -81,43 +95,29 @@ int main() {
 
 	// Initialize wait time mutex
 	pthread_mutex_init(&overallWaitMutex, NULL);
-
-	// Declare variables for file parsing (array of cutomer details)
-	int numCustomers;
 	
-	struct customerInfo *customerDetails;
+	// Initialize customers served mutex
+	pthread_mutex_init(&customersServedMutex, NULL);
+	
 	
 	// Read customer information from a file and store the info in an array
-	customerDetails = readFile("customers.txt", &numCustomers);
-
+	customerDetails = readFile("customers.txt", &totalCustomers);
+	
+	// Record the start time of the simulation
+	gettimeofday(&initTime, NULL);
+	
+/* ============================ THREAD CREATION ============================ */
 	pthread_t customerThread[MAX_CUSTOMERS];
     int clerkIndex[NCLERKS];
     int customerIndex[MAX_CUSTOMERS];
-	
-	/*
-	printf("===================== DEBUG =====================\n");
-	printf("Parsed Customers START:\n");
 
-	for(int i = 0; i < numCustomers; i++) {
-    printf("Customer %d | class %d | arrival %d | service %d\n",
-        customerDetails[i].userID,
-        customerDetails[i].classType,
-        customerDetails[i].arrivalTime,
-        customerDetails[i].serviceTime);
-	}
-	printf("Parsed Customers DONE!\n");
-	*/ 
-
-	// Free memory
-    // free(customerDetails);
-
-	// Create clerks threads
-    for (int i = 0; i < NCLERKS; i++) {
+	// Clerk threads    
+	for (int i = 0; i < NCLERKS; i++) {
         clerkIndex[i] = i;
-        pthread_create(&clerkThreads[i], NULL, clerkEntry, &clerkIndex[i]);
+        pthread_create(&clerkThread[i], NULL, clerkEntry, &clerkIndex[i]);
     }
 
-    // Create customer threads
+    // Customer threads
     for (int i = 0; i < totalCustomers; i++) {
         customerIndex[i] = i;
         pthread_create(&customerThread[i], NULL, customerEntry, &customerIndex[i]);
@@ -128,7 +128,10 @@ int main() {
         pthread_join(customerThread[i], NULL);
     }
 
-	
+	// Wait for clerks
+	for (int i = 0; i < NCLERKS; i++) {
+    	pthread_join(clerkThread[i], NULL);
+	}
 
 
 	
@@ -137,6 +140,8 @@ int main() {
 	
 	// calculate the average waiting time of all customers
 	
+	// Free memory
+    free(customerDetails);
 
 	return 0;
 }
@@ -144,7 +149,7 @@ int main() {
 /* ==================================== HELPER FUNCTIONS ==================================== */
 
 
-// Read customer information from a file
+/* Read customer information from a file */
 struct customerInfo* readFile(char *filename, int *numCustomers){
 
 	// Open file and exit if empty
@@ -178,84 +183,109 @@ struct customerInfo* readFile(char *filename, int *numCustomers){
     return customerList;
 }
 
-// Enqueue the customer's index (from customerDetails array) to the BUSINESS/ECONOMY queue
-void enqueue(int queueType, int customerIndex) {
 
-    // Add customer index to queue and increment the length
-    queue[queueType][back[queueType]] = customerIndex;
-    back[queueType] = (back[queueType] + 1) % MAX_CUSTOMERS;
-    queueLength[queueType]++;
-}
-
-// Dequeue the customer's index (from customerDetails array) from the BUSINESS/ECONOMY queue
-int dequeue(int queueType) {
-
-	int customerIndex = queue[queueType][front[queueType]];
-
-    // Remove customer index from queue and decrement the length
-    front[queueType] = (front[queueType] + 1) % MAX_CUSTOMERS;
-    queueLength[queueType]--;
-
-	// Return index of dequeued customer
-	return cutomerIndex;
-}
-
-// Clerk chooses which queue to serve next
-int serveQueue() {
-
-	if (queueLength[BUSINESS] > 0) {
-		return BUSINESS;
-	}
-
-	if (queueLength[ECONOMY] > 0) {
-		return ECONOMY;
-	}
-
-	// Both queues are empty
-	return EMPTY;
-}
-
-
-// ============================ TO IMPLEMENT ===========================
-
-// Get the current simulation time
+/* Get the current simulation time */
 double getCurrentTime() {
 
-	// returns the current simulation time in seconds 	
-	return 0.0;
+	// Get the current time 
+	struct timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+
+	// Calculate time passed since the start of simulation
+    double seconds = (currentTime.tv_sec - initTime.tv_sec);
+    double microseconds = (currentTime.tv_usec - initTime.tv_usec) / 1000000.0;
+    return seconds + microseconds;
 }
 
-// Add customer's wait time to overallWaitTime
+
+/* Add customer's wait time to overallWaitTime */
 void updateWaitTime(double overallWaitTime) {
 
-	// updates the time and returns nothing
+	// Lock mutex to update overall wait time
+    pthread_mutex_lock(&overallWaitMutex);
+
+	// Update overall wait time
+    overallWaitTime += waitTime;
+
+	// Unlock mutex after updating overall wait time
+    pthread_mutex_unlock(&overallWaitMutex);
 }
 
 
+/* ==================================== CUSTOMER FUNCTION ==================================== */
 
-
-
-
-// Enter customer threads and print messages
+/* Enter customer threads and print messages */
 void* customerEntry(void *customerArg) {
 
-	// Cast customerID to int type from void
-	int customerID = *(int *)customerArg;
+	// Cast input to int from type void
+	// this variable is the index of the current customer from the customerDetails list (from readfile())
+	int customerDetailsIndex = *(int *)customerArg;
 
-	// Sleep
-	// Call enqueue()
-	// Wait
-	// Receive service from clerk
-	// Record wait time
-	//
+	// Sleep until arrival time
+	usleep(customerDetails[customerDetailsIndex].arrivalTime * 100000);
+
+	// Customer arrival
+	int customerID = customerDetails[customerDetailsIndex].userID;
+	int queueType = customerDetails[customerDetailsIndex].classType;
+	int clerkID;
+
+	printf("CUSTOMER ARRIVES: Unique customer ID is %d. \n", customerID);
+
+	// Enter the queue
+	pthread_mutex_lock(&queueMutex[queueType]);
+	enqueue(queueType, customerDetailsIndex);
+	printf("CUSTOMER ENTERS QUEUE: Successfully entered the %s queue and length is now %d customers. \n",
+			queueType == BUSINESS ? "BUSINESS" : "ECONOMY",
+			queueLength[queueType]);
+
+	// Wait for clerk signal until customer is selected
+	while (1) {
+		pthread_cond_wait(&queueCond[queueType], &queueMutex[queueType]);
+
+		if (queue[queueType][front[queueType]] == customerDetailsIndex && winnerSelected[queueType] == FALSE) {
+			dequeue(queueType);
+			winnerSelected[queueType] = TRUE;
+			clerkID = queueStatus[queueType];
+			break;
+		}
+	}
+
+	pthread_mutex_unlock(&queueMutex[queueType]);
+
+	double startTime = getCurrentTime();
+	double arrivalTime = customerDetails[customerDetailsIndex].arrivalTime / 10.0;
+	double waitTime = startTime - arrivalTime;
+	updateWaitTime(waitTime);
+
+	// Clerk starts serving customer
+	printf("CLERK STARTS SERVING CUSTOMER: Clerk number %d is serving customer %d. | Start time: %2f. \n",
+			clerkID, customerID, getCurrentTime());
+
+	// Simulate time it takes to service a customer
+    usleep(customerDetails[customerDetailsIndex].serviceTime * 100000);
+
+    // Clerk finishes serving customer
+    printf("CLERK FINISHES SERVING CUSTOMER: Clerk %d finishes serving customer %d. | End time: %.2f. \n",
+            clerkID, customerID, getCurrentTime());
+
+	// Increment number of customers served
+	pthread_mutex_lock(&customersServedMutex);
+	customersServed++;
+	pthread_mutex_unlock(&customersServedMutex);
+
+
+    // Notify the clerk that service is finished
+    pthread_mutex_lock(&clerkMutex[clerkID]);
+    pthread_cond_signal(&clerkCond[clerkID]);
+    pthread_mutex_unlock(&clerkMutex[clerkID]);
+
+    pthread_exit(NULL);
 }
 
 
+/* ==================================== CLERK FUNCTION ==================================== */
 
-
-
-
-// Enter clerk threads and print messages
+/* Enter clerk threads */
 void *clerkEntry(void * clerkArg) {
 
 	// Cast clerkID to int type from void
@@ -263,11 +293,21 @@ void *clerkEntry(void * clerkArg) {
 
 	while(TRUE) {
 		
+		// Lock customer count and check if all customers have been served
+		pthread_mutex_lock(&customersServedMutex);
+		if (customersServed >= totalCustomers) {
+			// Break loop and end thread
+			pthread_mutex_unlock(&customersServedMutex);
+			break;
+		}
+		// Unlock customer count
+		pthread_mutex_unlock(&customersServedMutex);
+
 		// Call serveQueue() to determine which queue to service next
 		int serveNow = serveQueue();
 
 		// Wait if both queues are empty
-		if (serveNow == -1) {
+		if (serveNow == EMPTY) {
 			usleep(1000);
 			continue;
 		}
@@ -294,6 +334,7 @@ void *clerkEntry(void * clerkArg) {
         pthread_mutex_unlock(&clerkMutex[clerkID]);
 	}
 
+	// Exit if all customers have been served
 	pthread_exit(NULL);
 }
 
