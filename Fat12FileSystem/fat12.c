741931a7b8b311fc1c76b7c8f3e585747653bc5b
Mei Stoesz-Gouthro <mei.stoesz.gouthro@gmail.com>
2026-03-22 00:01:16 -0700

    Add files via upload

---
+++ Fat12FileSystem/fat12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "fat12.h"
+
+void readBootSector(FILE *fp) {
+    // implementation
+}
+
+int getNextCluster(int cluster, FILE *fp) {
+    // implementation
+    return 0;
+}
